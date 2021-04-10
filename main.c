7c780ecd9a9d730c54d013f54706ea90213d860b
gsevimli17 <>
2021-04-10 11:28:58 +0000

    fixed parameters are declared

--- main.c
+++ main.c
@@ -2,31 +2,45 @@
  * The main aim of this assigment is getting familiar with dynamic arrays and heap management. 
  *  
  *  Please write your name and Student ID as comment below.
- *  Name and Surname:               Student ID:
+ *  Name and Surname:  Gökçe Sevimli             Student ID: 63992
  */
 
 
 
 #include <stdio.h>
 #include <stdlib.h>
+#define NUM_COLS 10
+#define NUM_ROWS 10
+
+
 
 
 
 int main( int argc, char *argv[] )  {
+int N = 10;
+int Grid_World[N][N];
+double Fatality_Rate = 3/100;
+double Recover_Rate = 1- (3/100);
+double Initial_Active_Cases = 5/100;
 
-   printf("Program name %s\n", argv[0]);
+
+
+
+
+
+ //  printf("Program name %s\n", argv[0]);
  
-   if( argc == 5 ) {
-	printf("The first argument supplied is %d\n", atoi(argv[1]));
-	printf("The second argument supplied is %d\n", atoi(argv[2]));
-  	printf("The third argument supplied is %d\n", atoi(argv[3]));
-	printf("The forth argument supplied is %d\n", atoi(argv[4]));
-   }
-   else if( argc > 5 ) {
-      printf("Too many arguments supplied.\n");
-   }
-   else {
-      printf("Expected 4 arguments but only %d argument passed.\n", argc-1);
-   }
+ // if( argc == 5 ) {
+	//printf("The first argument supplied is %d\n", atoi(argv[1]));
+	//printf("The second argument supplied is %d\n", atoi(argv[2]));
+  	//printf("The third argument supplied is %d\n", atoi(argv[3]));
+	//printf("The forth argument supplied is %d\n", atoi(argv[4]));
+   //}
+   //else if( argc > 5 ) {
+  //  printf("Too many arguments supplied.\n");
+   //}
+   //else {
+     // printf("Expected 4 arguments but only %d argument passed.\n", argc-1);
+   //}
       
 }
